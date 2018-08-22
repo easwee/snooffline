64ca98df2f0a51a45faa26461029bc3b2c7b6ff9
Adam Giacomelli <adam.giacom@gmail.com>
2018-08-22 10:53:34 +0200

    Added placeholder pixel data generator

--- src/engine.js
+++ src/engine.js
@@ -18,6 +18,28 @@ Engine.prototype.init = function() {
   this.canvas.height = window.innerHeight;
   this.ctx = this.canvas.getContext("2d");
 
+  this.roadPixelData = this.ctx.createImageData(this.canvas.width, this.canvas.height);
+  for (var x=0; x<this.canvas.width; x++) {
+    for (var y=0; y<this.canvas.height; y++) {
+        // Get the pixel index
+        var pixelindex = (y * this.canvas.width + x) * 4;
+
+        // Generate a xor pattern with some random noise
+        var red = ((x) % 256) ^ ((y) % 256);
+        var green = ((2*x) % 256) ^ ((2*y) % 256);
+        var blue = 50 + Math.floor(Math.random()*100);
+
+        // Rotate the colors
+        blue = (blue) % 256;
+
+        // Set the pixel data
+        this.roadPixelData.data[pixelindex] = red;     // Red
+        this.roadPixelData.data[pixelindex+1] = green; // Green
+        this.roadPixelData.data[pixelindex+2] = blue;  // Blue
+        this.roadPixelData.data[pixelindex+3] = 255;   // Alpha
+    }
+}
+
   this.loop();
 };
 
@@ -32,10 +54,15 @@ Engine.prototype.loop = function() {
     t.then = t.now - (t.delta % t.interval);
     // draw and update here
     this.drawBackground();
+    this.drawRoad();
   }
 };
 
 Engine.prototype.drawBackground = function() {
   this.ctx.fillStyle = "black";
   this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+}
+
+Engine.prototype.drawRoad = function() {
+  this.ctx.putImageData(this.roadPixelData, 0, 0);
 }
