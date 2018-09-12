e13586b1bfa64a1b507a17f99f86434d7d085060
Adam Giacomelli <adam.giacom@gmail.com>
2018-09-12 11:31:15 +0200

    Minifed

--- src/utils.js
+++ src/utils.js
@@ -8,27 +8,25 @@ window.collision = function collision(c1, c2) {
   var dist = c1.radius + c2.radius;
 
   return dx * dx + dy * dy <= dist * dist;
-}
+};
 
 /**
  * Get point on line at y
  * Usage: pointAtY({x: 10, y: 20}, {x: 150, y: 250}, y);
  */
 window.pointAtY = function pointAtY(p1, p2, y) {
-  if((p1.x - p2.x) === 0) return {x: p1.x, y: y};
+  if (p1.x - p2.x === 0) return { x: p1.x, y: y };
   const k = (p1.y - p2.y) / (p1.x - p2.x);
   const n = p1.y - k * p1.x;
   return { x: (y - n) / k, y: y };
-}
+};
 
 /**
  * Get point on line at x
  * Usage: pointAtY({x: 10, y: 20}, {x: 150, y: 250}, y);
  */
 window.pointAtX = function pointAtX(p1, p2, x) {
-  
-    const k = (p1.y - p2.y) / (p1.x - p2.x);
-    const n = p1.y - k * p1.x;
-    return { x: x, y: x * k + n };
-  
-}
+  const k = (p1.y - p2.y) / (p1.x - p2.x);
+  const n = p1.y - k * p1.x;
+  return { x: x, y: x * k + n };
+};
